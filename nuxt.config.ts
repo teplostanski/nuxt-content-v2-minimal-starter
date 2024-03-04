9f73c3580411523b8c00b9e60de836be863f6103
teplostanski <teplostanski@yandex.ru>
2024-03-04 21:01:14 +0300

    test deploy 10

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  //ssr: false,
+  //app: {
+  //  baseURL: '/nuxt-content-app-V2/',
+  //},
   devtools: { enabled: true },
   modules: ['@nuxt/content', 'nuxt-content-assets'],
   hooks: {
@@ -8,9 +12,5 @@ export default defineNuxtConfig({
       // Workaround for https://github.com/nuxt/cli/issues/169
       process.exit(0);
     },
-  },
-  ssr: false,
-  app: {
-    baseURL: '/nuxt-content-app/',
   },
 });
