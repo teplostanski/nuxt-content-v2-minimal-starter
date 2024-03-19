80840b139d591a7de281157c7c2f0c711c15ccb1
Irog Teplostanski <teplostanski@yandex.ru>
2024-03-19 16:15:56 +0300

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,6 @@ import { defineNuxtConfig } from 'nuxt/config';
 
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  ssr: true,
-  routeRules: {
-    "/ssg": { prerender: false },
-  },
   modules: ['@nuxt/content', 'nuxt-content-assets', '@nuxtjs/i18n', '@pinia/nuxt'],
   hooks: {
     close: () => {
