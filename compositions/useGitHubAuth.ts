242330d3928dbfcc8ff08a75d7ed8c2316640718
teplostanski <teplostanski@yandex.ru>
2024-03-25 04:46:27 +0300

    test deploy #2

--- compositions/useGitHubAuth.ts
+++ compositions/useGitHubAuth.ts
@@ -1,8 +1,8 @@
-// compositions/useGitHubAuth.ts
+// compositions/useGitHubAuth.js
 export default function useGitHubAuth() {
   const authenticateWithGitHub = () => {
-    const clientId = '6b23ac4984f4163d5509';
-    const redirectUri = 'https://blog.teplostanski.dev/';
+    const clientId = '6b23ac4984f4163d5509'; // Замените на ваш Client ID
+    const redirectUri = 'https://blog.teplostanski.dev/'; // Замените на URL вашего приложения на GitHub Pages
     const githubOAuthUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=repo&response_type=token`;
 
     window.location.href = githubOAuthUrl;
@@ -16,9 +16,9 @@ export default function useGitHubAuth() {
       return;
     }
 
-    const clientId = '6b23ac4984f4163d5509';
-    const clientSecret = '56210b4d0d2b8bb25cca985647dccd0af4e16c12';
-    const redirectUri = 'https://blog.teplostanski.dev/';
+    const clientId = '6b23ac4984f4163d5509'; // Замените на ваш Client ID
+    const clientSecret = 'c41cd92f37d42c29a4b9cc6e779c2c086a0f382d'; // Замените на ваш Client Secret
+    const redirectUri = 'https://blog.teplostanski.dev/'; // Замените на URL вашего приложения на GitHub Pages
 
     fetch(`https://github.com/login/oauth/access_token`, {
       method: 'POST',
@@ -37,7 +37,21 @@ export default function useGitHubAuth() {
     .then(data => {
       const accessToken = data.access_token;
       console.log('Access Token:', accessToken);
-      // Теперь у вас есть accessToken для использования с API GitHub
+      // Используйте accessToken для доступа к API GitHub
+      // Например, получение информации о репозитории
+      fetch('https://api.github.com/repos/teplostanski/uncrunch', {
+        headers: {
+          'Authorization': `token ${accessToken}`,
+        },
+      })
+      .then(response => response.json())
+      .then(repoData => {
+        console.log('Repository Data:', repoData);
+        // Теперь у вас есть данные о конкретном репозитории
+      })
+      .catch(error => {
+        console.error('Failed to fetch repository data:', error);
+      });
     })
     .catch(error => {
       console.error('Failed to get GitHub access token:', error);
